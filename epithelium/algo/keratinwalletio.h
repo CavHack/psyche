68848db8c8eb355df8904d6a47038dc221242516
Karl Whitford <karl@blackesquirel.com>
2018-05-18 21:36:13 -0400

    Keratin Wallet
    queue and parse the state ambiguous lamport tree within an instance of keratinwalletio. Hypergraph is ready to sign and encrypt.

---
+++ epithelium/algo/keratinwalletio.h
@@ -0,0 +1,10 @@
+/*Venire Labs Inc (c) 2018
+ */
+
+#ifndef __KERATINWALLETIO_H_
+#define __KERATINWALLETIO_H_
+
+int keratinwalletio(unsigned char *, size_t, unsigned char *, size_t, unsigned char *);
+
+/*vim: set ts=2 sw=2 et ai si:*/
+#endif
--- epithelium/hypergraph.h
+++ epithelium/hypergraph.h
@@ -20,6 +20,24 @@ struct hypergraph_pk {
 struct hypergraph_sign {
   struct tree rand;
   struct solomonkey_sign op_sign;
-  struct 
-}
+  struct lamportBlock_sign lamportBlock[HYPERGRAPH_d];
+#if HYPERGRAPH_c > 0
+  struct tree auth[HYPERGRAPH_c];
+#endif
+};
+
+int hypergraph_gensk(struct hypergraph_sk *sk);
+
+int hypergraph_genpk(const struct hypergraph_sk *sk, struct hypergraph_pk *pk);
+
+int hypergraph_sign (const struct hypergraph_sk *sk, struct hypergraph_sign *sign, const struct tree *msg);
+
+int hypergraph_verify(const struct hypergraph_pk *pk,
+		      const struct hypergraph_sign *sign,
+		      const struct tree *msg);
+
+/*Serialization*/
+int hypergraph_loadsign(struct hypergraph_sign *sign, const uint8_t *_sign, size_t _len);
+
+int gravity_signcmp(const struct hypergraph_sign *sign1, const struct hypergraph_sign *sign2);
 
