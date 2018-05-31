cbdacda37e7a2a9e747de963848d7837b8979404
Karl Whitford <karl@blackesquirel.com>
2018-05-31 17:58:36 -0400

    Precomparison hashing
    Check debug folder

---
+++ epithelium/lbuilding.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) Venire Labs All Rights Reserved 2018
+ */
+
+#include "lbuilding.h"
+
+void lbuilding (struct tree *buf, int cound, struct tree *root) {
+
+  void lbuilding (struct tree *buf, int count, struct tree *root) {
+
+    struct tree *src = &buf[count];
+    struct tree *dst = &buf[0];
+    struct tree *tmp;
+   
+    int newCount;
+
+
+
+  }
+
+
+}
+
+
