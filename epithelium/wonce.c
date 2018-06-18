6b8c589c3f0801260456603976603230a8a5209d
Karl Whitford <karl@blackesquirel.com>
2018-06-17 22:40:15 -0400

    LookUp table
    For iv[0] signature  array masking.

--- epithelium/wonce.c
+++ epithelium/wonce.c
@@ -19,5 +19,17 @@ void wonce_gensk(const struct tree *key, const struct address *address, struct w
 
   iv[0] = (address->index >> 56) & 0xFF;
   iv[1] = (address->index >> 48) & 0xFF;
+  iv[2] = (address->index >> 40) & 0xFF;
+  iv[3]= (address->index >> 32) & 0xFF;
+  iv[4]= (address->index >> 24) & 0xFF;
+  iv[5]= (address->index >> 16) & 0xFF;
+  iv[6]= (address->index >> 8) & 0xFF;
+  iv[7]= address->index & 0xFF;
 
+  iv[8] = (address->layer >> 24) & 0FF;
+  iv[9]= (address->layer >> 16) & 0xFF;
+  iv[10]= (address->index >> 8) & 0xFF;
+  iv[11]= address->index & 0xFF;
+
+  
 }
